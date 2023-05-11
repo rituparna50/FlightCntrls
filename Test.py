9d9123042ee3e1db5ad42b6056ffd167fe514e38
rituparna50 <rituparna50@gmail.com>
2023-05-10 19:11:26 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,25 @@
 import numpy as np
 
-coeffs = [1, 3, -2, 1]
+#Defining the coefficients of the matrix
+coeffs = [1, 3, 2, 1]
 
+#Finding roots (poles) of the characteristic equation
 roots = np.roots(coeffs)
+
+#printing the poles of the system
 print("Poles of the system:")
 for pole in roots:
     print(pole)
+
+    #Defining the matrix
+    # A = [0 1 0]
+    #     [0 0 1]
+    #     [-1 -2 -3]
+A = np.array([[0, 1, 0],
+              [0, 0, 1],
+              [-1, -2, -3]])
+
+# To get eigenvalues of matrix A
+eigenvalues = np.linalg.eigvals(A)
+print("Eigenvalues of matrix A:")
+print(eigenvalues)
